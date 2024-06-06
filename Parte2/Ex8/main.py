2434f6acf955bde0c1d38990e541034d4d1dcfca
Miguel Oliveira <m.riem.oliveira@gmail.com>
2024-06-06 19:43:32 +0100

    Ex8

--- Parte2/Ex8/main.py
+++ Parte2/Ex8/main.py
@@ -7,54 +7,55 @@ import matplotlib.pyplot as plt
 
 def main():
 
+    # --------------------------
+    # Initialization
+    # --------------------------
     # filename = '/home/mike/sharedVBWindows/MVA_6-24/MVA_24-25/Parte1/imagens/grey_lake.jpg'
-    filename = '/home/mike/sharedVBWindows/MVA_6-24/MVA_24-25/Parte1/imagens/chess.png'
-
+    filename = '/home/mike/sharedVBWindows/MVA_6-24/MVA_24-25/Parte2/imagens/ireland-06-02.tif'
     if not os.path.isfile(filename):
         print('filename ' + filename + ' does not exist')
 
     # Load the image
     image = cv2.imread(filename,  cv2.IMREAD_GRAYSCALE)
+    h, w = image.shape
 
-    # compute histogram
+    image_stretched = image*0
+
+    # --------------------------
+    # Execution
+    # --------------------------
+
+    # Stretch the histogram
+    minimum, maximum, _, _ = cv2.minMaxLoc(image)
+
+    for x in range(0, w):
+        for y in range(0, h):
+            image_stretched[y, x] = (image[y, x] - minimum) / (maximum-minimum)*255
+
+    # Compute histograms --------------
     nbins = 128
 
     # how to compute the values of xs
     # linspace returns the centers of the bins, which we use for plotting
     xs = np.round(np.linspace(0, 255, nbins))
-
     ys = cv2.calcHist([image], [0], None, [nbins], (0, 256), accumulate=False)
+    ys_stretched = cv2.calcHist([image_stretched], [0], None, [nbins], (0, 256), accumulate=False)
 
-    print(ys)
-    print(len(ys))
-
-    print(xs)
-    print(len(xs))
+    # --------------------------
+    # Visualization
+    # --------------------------
 
     # Display the image
     cv2.imshow('Image', image)
-
-    # thresholding
-
-    # Get white and grey squares
-    bw = (image > 90).astype(np.uint8)*255
-    print(bw.dtype)
-    cv2.imshow('gray and white', bw)
-
-    # Get black and grey squares
-    bw2 = (image < 190).astype(np.uint8)*255
-    cv2.imshow('black and gray', bw2)
-
-    # Get black and grey squares
-    # bw3 = (image > 190).astype(np.uint8)*255
-    bw3 = (np.logical_not(image < 190)).astype(np.uint8)*255
-    cv2.imshow('whites', bw3)
+    cv2.imshow('Image_stretched', image_stretched)
 
     # Wait for the user to press a key
     cv2.waitKey(100)
 
     # matplotlib
-    plt.plot(xs, ys)
+    plt.plot(xs, ys, 'b')
+    plt.plot(xs, ys_stretched, 'r')
+
     plt.show()
 
     # Close all windows
